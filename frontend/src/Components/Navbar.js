65280af6ed3cd838f479b61f6d9301554d432ed5
Gohar Ali <Bcsf19a010@pucit.edu.pk>
2023-04-01 10:53:45 +0500

    changed Navbar a little bit

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -15,7 +15,10 @@ export default function Navbar(props) {
       className="navbar navbar-expand-lg bg-light"
       style={{ padding: "0px", width: "100%" }}
     >
-      <div className="container-fluid" style={{ backgroundColor: "black" }}>
+      <div
+        className="container-fluid"
+        style={{ backgroundColor: "black", paddingTop: "30px" }}
+      >
         <Link className="navbar-brand text-dark" to="/">
           <img src={imagae} alt="Logo" />
         </Link>
@@ -34,7 +37,7 @@ export default function Navbar(props) {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <Link
-                style={{ color: "white", fontWeight: "bold" }}
+                style={{ color: "white", fontWeight: "bold", fontSize: "25px" }}
                 className="nav-link"
                 aria-current="page"
                 to="/about"
