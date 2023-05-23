a34779ac8c369a4d7195d7a28265b6c352a91ef3
aliHaidar <bcsf19a020@pucit.edu.pk>
2023-05-23 08:24:46 +0500

    userpanel added

--- frontend/src/Components/UserPanel/UserPanel.js
+++ frontend/src/Components/UserPanel/UserPanel.js
@@ -3,22 +3,30 @@ import "./userPanel.css"
 export default function UserPanel ()
 {
     return(
-        
-            <div className='container'>
+        <>
+            <div className='userpanel'>
+                <center><h1 className='qoute'><font color="orangered">MOVE IT</font> Or LOSE IT</h1></center>
                 <div className='trainer'>
-                    <h1>Hello</h1>
-                    <ul className='trainer'>
+                   <center><h3>Welcome to the Gym</h3></center> 
+                    <ul>
                         <li>
                             <a href="/Home"><img src={`${process.env.PUBLIC_URL}/images/b.png`} ></img></a>
                         </li>
-                        <li>
-                            Welcome to the Gym
-                        </li>
+                        
                     </ul>
                 </div>
+                <hr></hr>
+                <div  className='diet'  >
+                    <ul >
+                        <li>
+                            <a href="/Home"><img className='nImg' src={`${process.env.PUBLIC_URL}/images/download.jpg`} ></img></a>
+                        </li>
+                        <li>Welcome to the Gym</li>
+                    </ul>
+                </div>
+                <hr></hr>
                 <div  className='ebooks'  >
-                <h1>Hello</h1>
-                    <ul className='ebooks'>
+                    <ul >
                         <li>
                             <a href="/Home"><img className='nImg' src={`${process.env.PUBLIC_URL}/images/download.jpg`} ></img></a>
                         </li>
@@ -29,5 +37,6 @@ export default function UserPanel ()
                 </div>
 
             </div>
+    </>    
     )
 }
