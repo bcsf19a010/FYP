0be1a366f17c5962c694e1153aae2bf9c97920e9
Gohar Ali <Bcsf19a010@pucit.edu.pk>
2023-06-14 12:01:27 +0500

    exercise done

--- backend/Controllers/AdminController.js
+++ backend/Controllers/AdminController.js
@@ -9,8 +9,21 @@ const user = require("../models/user");
 // const attendance = require("../models/Attendance");
 // const validator = require("validator");
 const jwt = require("jsonwebtoken");
+const path = require("path");
 //const employee = require("../models/Employee");
 const admin = require("../models/Admin");
+const multer = require("multer");
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     // Set the destination folder based on the category
+//     cb(null, path.join(__dirname, `../../frontend/public/videos/`));
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+const upload = multer({});
 
 const createToken = (_id) => {
   return jwt.sign({ _id }, "SECRETKey12345678", { expiresIn: "3d" });
@@ -54,21 +67,99 @@ router.get("/viewUsers", async (req, resp) => {
 
 // **************************** Exercise Routes **********************
 
-router.post("/addExercise", async (req, resp) => {
+// router.post("/addExercise", upload.single("file"), async (req, resp) => {
+//   try {
+//     const { name, descriptions, category } = req.body;
+//     const file = req.file;
+
+//     //const { file, name, descriptions, category } = req.body;
+//     console.log("\n\n", req.body);
+//     console.log("Received form data:");
+//     console.log("File:", file);
+//     console.log("Name:", name);
+//     console.log("Descriptions:", descriptions);
+//     console.log("Category:", category);
+
+//     // Save the file to the desired folder
+//     const filePath = path.join(
+//       __dirname,
+//       `../../frontend/public/videos/${category}`
+//     );
+//     console.log(filePath);
+//     await file.mv(filePath);
+//     // console.log("\nadd exercise\n", req.body);
+//     // let ex = new exercise(req.body);
+//     // console.log("\n\nexercise", ex);
+//     // let result = await ex.save();
+//     resp.status(200).json({ result: "result" });
+//     //const {exReferece,bodyPart,name,description} = req.body;
+//   } catch (error) {
+//     resp.status(400).json({ error: error.message });
+//   }
+// });
+
+router.post("/addExercise", upload.single("file"), async (req, resp) => {
   try {
-    const { file, name, descriptions, category } = req.body;
-    console.log("\n\n", req.body);
-    console.log("Received form data:");
-    console.log("File:", file);
-    console.log("Name:", name);
-    console.log("Descriptions:", descriptions);
-    console.log("Category:", category);
-    // console.log("\nadd exercise\n", req.body);
-    // let ex = new exercise(req.body);
-    // console.log("\n\nexercise", ex);
-    // let result = await ex.save();
-    resp.status(200).json({ result: "result" });
-    //const {exReferece,bodyPart,name,description} = req.body;
+    var { name, descriptions, category } = req.body;
+    const file = req.file;
+
+    // storage.destination = (req, file, cb) => {
+    //   // Set the destination folder based on the category
+    //   cb(
+    //     null,
+    //     path.join(__dirname, `../../frontend/public/videos/${category}`)
+    //   );
+    // };
+    // const ex = new exercise(file.originalname, category, name, descriptions);
+    // console.log(ex);
+
+    // Initialize multer upload
+    const storage = multer.diskStorage({
+      destination: function (req, file, cb) {
+        // Set the destination folder based on the category
+        cb(
+          null,
+          path.join(__dirname, `../../frontend/public/videos/${category}`)
+        );
+      },
+      filename: function (req, file, cb) {
+        cb(null, file.originalname);
+      },
+    });
+
+    upload.storage = storage;
+
+    // console.log("\n\n", req.body);
+    // console.log("Received form data:");
+    // console.log("File:", file);
+    // console.log("Name:", name);
+    // console.log("Descriptions:", descriptions);
+    // console.log("Category:", category);
+    const description = descriptions.split(",");
+
+    const ex = new exercise({
+      exReferece: file.originalname,
+      bodyPart: category,
+      name: name,
+      description: description,
+    });
+    const result = await ex.save();
+
+    // Save the file to the desired folder
+    // const filePath = path.join(
+    //   __dirname,
+    //   `../../frontend/public/videos/${category}`,
+    //   file.originalname
+    // );
+    const filePath = path.join(
+      __dirname,
+      `../../frontend/public/videos/${category}`,
+      file.originalname
+    );
+
+    await file.mv(filePath);
+
+    resp.status(200).json({ result });
   } catch (error) {
     resp.status(400).json({ error: error.message });
   }
@@ -84,9 +175,10 @@ router.get("/getExercises", async (req, resp) => {
 });
 
 //delete Exercise
-router.delete("deleteExercise/:id", async (req, resp) => {
+router.delete("/deleteExercise/:id", async (req, resp) => {
   const { id } = req.params;
   try {
+    //console.log(id);
     const ex = await exercise.findByIdAndDelete({ _id: id });
     return resp.status(200).json(ex);
   } catch (error) {
--- frontend/src/Components/AdminPanel/adminPanel.js
+++ frontend/src/Components/AdminPanel/adminPanel.js
@@ -1,32 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./adminPanel.css";
 
 function AdminPanel() {
-
   return (
-    
-   <>
-   
-   <div className='back_pic'>
-   
-      <div className="ahmad_container">
-        <div className='exe'>
-          <a href="./exercise">        
-          <img src="/images/fitness.png" alt="Photo 1"  />
-          <h3> Exercises</h3></a>
-        </div>
-        <div className='eBooks'>    
-          <a href="">
-            <img src="/images/book.gif"alt="Photo 2" />
-             <h3>E-books</h3></a>    
-        </div>
-        <div className='owners'>
-          <a href="">
-          <img src="/images/boss.png" alt="Photo 3"  />
-          <h3>Owners</h3></a>
+    <>
+      <div className="back_pic">
+        <div className="ahmad_container">
+          <div className="exe">
+            <a href="./viewExercise">
+              <img src="/images/fitness.png" alt="Photo_1" />
+              <h3> Exercises</h3>
+            </a>
+          </div>
+          <div className="eBooks">
+            <a href="#">
+              <img src="/images/book.gif" alt="Photo_2" />
+              <h3>E-books</h3>
+            </a>
+          </div>
+          <div className="owners">
+            <a href="#">
+              <img src="/images/boss.png" alt="Photo 3" />
+              <h3>Owners</h3>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
